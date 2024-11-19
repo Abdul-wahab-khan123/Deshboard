ccbab3b94b2cbe460b9ea14788bdbd40c0398bbe
Abdul-wahab-khan123 <wahabgaming975@gmail.com>
2024-11-20 02:36:51 +0500

    done

--- js/custom.js
+++ js/custom.js
@@ -16,20 +16,9 @@ sideBox = document.querySelector(".side-box").querySelectorAll("a")
 sideBox.forEach(element =>{
     element.addEventListener("click", function(){
         sideBox.forEach(nav=>nav.classList.remove("active"))
-        location.reload();
         this.classList.add("active")
     })
 }); 
-
-// const links = document.querySelectorAll('a');
-
-//     links.forEach(link => {
-//       link.addEventListener('click', function (e) {
-//         e.preventDefault(); // Prevent the default link action
-//         localStorage.setItem('activeLink', this.id); // Save the clicked link ID
-//         location.reload(); // Reload the page
-//       });
-//     });
 
 $(document).ready( function () {
     $('#myTable').DataTable();
